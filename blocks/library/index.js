0c3f78146b71c95ee321c46069d960258ad5944a
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2018-01-18 14:45:42 +0100

    Blocks: Refactor embeds to be expressed as name and setting pair

--- blocks/library/index.js
+++ blocks/library/index.js
@@ -1,17 +1,14 @@
 /**
  * Internal dependencies
  */
+import * as audio from './audio';
+import * as embed from './embed';
 import { registerBlockType } from '../api';
 import { registerShortcodeBlock } from './shortcode';
 import { registerImageBlock } from './image';
 import { registerGalleryBlock } from './gallery';
 import { registerHeadingBlock } from './heading';
 import { registerQuoteBlock } from './quote';
-import {
-	registerEmbedBlock,
-	registerCommonEmbedBlocks,
-	registerOtherEmbedBlocks,
-} from './embed';
 import { registerListBlock } from './list';
 import { registerSeparatorBlock } from './separator';
 import { registerMoreBlock } from './more';
@@ -28,19 +25,23 @@ import { registerCoverImageBlock } from './cover-image';
 import { registerTextColumnsBlock } from './text-columns';
 import { registerVerseBlock } from './verse';
 import { registerVideoBlock } from './video';
-import * as audio from './audio';
 import { registerReusableBlock } from './block';
 import { registerParagraphBlock } from './paragraph';
 
 export const registerCoreBlocks = () => {
+	[
+		audio,
+		embed,
+		...embed.common,
+		...embed.others,
+	].forEach( ( { name, settings } ) => {
+		registerBlockType( name, settings );
+	} );
 	registerShortcodeBlock();
 	registerImageBlock();
 	registerGalleryBlock();
 	registerHeadingBlock();
 	registerQuoteBlock();
-	registerEmbedBlock();
-	registerCommonEmbedBlocks();
-	registerOtherEmbedBlocks();
 	registerListBlock();
 	registerSeparatorBlock();
 	registerMoreBlock();
@@ -57,7 +58,6 @@ export const registerCoreBlocks = () => {
 	registerTextColumnsBlock();
 	registerVerseBlock();
 	registerVideoBlock();
-	registerBlockType( audio.name, audio.settings );
 	registerReusableBlock();
 	registerParagraphBlock();
 };
